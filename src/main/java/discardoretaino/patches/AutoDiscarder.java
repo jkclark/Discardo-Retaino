8bb0fd7652addaee2a64d6794376d5573f93fc4f
Josh Clark <jclarktennis@gmail.com>
2023-07-15 19:11:37 +0200

    feat: auto-select card if all cards are the same

--- src/main/java/discardoretaino/patches/AutoDiscarder.java
+++ src/main/java/discardoretaino/patches/AutoDiscarder.java
@@ -3,13 +3,12 @@ package discardoretaino.patches;
 import basemod.ReflectionHacks;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
-import com.megacrit.cardcrawl.actions.common.DiscardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.screens.select.HandCardSelectScreen;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
 
 public class AutoDiscarder {
     public static String modID; //Edit your pom.xml to change this
@@ -25,17 +24,52 @@ public class AutoDiscarder {
     public static class CustomDiscardPatch {
         @SpirePostfixPatch()
         public static void Postfix(HandCardSelectScreen __instance, String msg, int amount, boolean anyNumber, CardGroup ___hand) {
-            logger.info("\n\n\nNow in hand card select screen\n\n");
             if (___hand != null) {
                 for (int cardIndex = 0; cardIndex < ___hand.group.size(); cardIndex++) {
                     logger.info("Card at index " + cardIndex + ":" + ___hand.group.get(cardIndex));
                 }
 
-                // For now, just try to select the first card in the hand
-                __instance.hoveredCard = ___hand.group.get(0);
-                ReflectionHacks.privateMethod(HandCardSelectScreen.class, "selectHoveredCard").invoke(__instance);
+                ArrayList<Integer> cardIndexesToDiscard = CustomDiscardPatch.getCardsToDiscard(___hand);
+                if (!cardIndexesToDiscard.isEmpty()) {
+                    __instance.hoveredCard = ___hand.group.get(cardIndexesToDiscard.get(0));
+                    ReflectionHacks.privateMethod(HandCardSelectScreen.class, "selectHoveredCard").invoke(__instance);
+                }
             }
         }
+
+        private static ArrayList<Integer> getCardsToDiscard(CardGroup hand) {
+            /* Determine which cards should be auto-selected for discard.
+
+               This function returns an array of numbers, each corresponding to an index
+               in the list of cards in the hand.
+
+               Logic for auto-discarding:
+               - If all cards are the same, we can choose any card(s).
+               - ...TODO
+             */
+            ArrayList<Integer> discardIndexes = new ArrayList<Integer>();
+
+            if (hand.group.isEmpty()) {
+                return discardIndexes;
+            }
+
+            if (isHandAllSameCard(hand)) {
+                discardIndexes.add(0);
+                return discardIndexes;
+            }
+
+            return discardIndexes;
+        }
+
+        private static boolean isHandAllSameCard(CardGroup hand) {
+            /* Check if every card in the hand has the same name. */
+            if (hand.group.isEmpty()) {
+                return true;
+            }
+            return hand.getCardNames().stream().distinct().count() == 1;
+        }
     }
+
+    // TODO: Custom patch for retain
 }
 
