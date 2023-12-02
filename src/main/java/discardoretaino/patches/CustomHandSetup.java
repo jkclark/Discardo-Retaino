eda26e28fe4ac5a69202382426f61b1a36c679df
Josh Clark <jclarktennis@gmail.com>
2023-12-02 14:22:18 +0100

    refactor: clean up custom-hand code

--- src/main/java/discardoretaino/patches/CustomHandSetup.java
+++ src/main/java/discardoretaino/patches/CustomHandSetup.java
@@ -5,8 +5,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.colorless.Purity;
 import com.megacrit.cardcrawl.cards.green.*;
-import com.megacrit.cardcrawl.cards.status.Burn;
-import com.megacrit.cardcrawl.cards.status.Dazed;
+import com.megacrit.cardcrawl.cards.purple.WindmillStrike;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 
 import java.util.ArrayList;
@@ -21,10 +20,11 @@ public class CustomHandSetup {
     public static class PostDraw {
         @SpirePostfixPatch()
         public static void Postfix(AbstractPlayer ___instance) {
+            /* This method adds cards to your hand for debugging. */
             if (testingOn) {
                 ArrayList<AbstractCard> cardsToAdd = new ArrayList<AbstractCard>();
 
-                // Special purity that allows us to discard whatever we want
+                // Special purity that allows us to exhaust whatever we want
                 Purity purity = new Purity();
                 purity.baseMagicNumber = 10;
                 purity.magicNumber = 10;
@@ -38,15 +38,25 @@ public class CustomHandSetup {
                 Strike_Green upgraded_strike_2 = new Strike_Green();
                 upgraded_strike_2.upgrade();
 
-                // Other cards for testing
-                cardsToAdd.add(new WellLaidPlans());
+                // Basic cards
+                cardsToAdd.add(new Strike_Green());
                 cardsToAdd.add(new Strike_Green());
                 cardsToAdd.add(upgraded_strike);
                 cardsToAdd.add(upgraded_strike_2);
-                cardsToAdd.add(new Dazed());
-                cardsToAdd.add(new Reflex());
-                cardsToAdd.add(new Survivor());
+                cardsToAdd.add(new Defend_Green());
 
+                // Discard
+//                cardsToAdd.add(new Survivor());
+//                cardsToAdd.add(new Reflex());
+//                cardsToAdd.add(new Tactician());
+
+                // Retain
+                cardsToAdd.add(new WellLaidPlans());
+                cardsToAdd.add(new WindmillStrike());
+
+                // Status/Curse
+//                cardsToAdd.add(new Dazed());
+//                cardsToAdd.add(new Doubt());
 
                 for (int cardIndex = 0; cardIndex < cardsToAdd.size(); cardIndex++) {
                     ___instance.hand.addToHand(cardsToAdd.get(cardIndex));
